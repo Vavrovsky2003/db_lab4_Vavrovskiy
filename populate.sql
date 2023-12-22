8bda744c6e54975ed4052dfd1a5b028bc0e10c07
Vavrovsky2003 <113840563+Vavrovsky2003@users.noreply.github.com>
2023-12-22 20:53:49 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,33 +1,33 @@
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('ANC','Ted Stevens Anchorage International Airport', 'Anchorage', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('ATL','Hartsfield-Jackson Atlanta International Airport', 'Atlanta', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('CLT','Charlotte Douglas International Airport', 'Charlotte', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('DEN','Denver International Airport', 'Denver', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('DFW','Dallas/Fort Worth International Airport', 'Dallas-Fort Worth', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('IAH','George Bush Intercontinental Airport', 'Houston', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('LAS','McCarran International Airport', 'Las Vegas', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('LAX','Los Angeles International Airport', 'Los Angeles', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('MIA','Miami International Airport', 'Miami', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('MSP','Minneapolis-Saint Paul International Airport', 'Minneapolis', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('PBI','Palm Beach International Airport', 'West Palm Beach', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('PDX','Portland International Airport', 'Portlands', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('SEA','Seattle-Tacoma International Airport', 'Seattle', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('SFO','San Francisco International Airport', 'San Francisco', 'USA');
-INSERT INTO Origin_Airport(IATA_code, Name, City, Country)
-VALUES('SLC','Salt Lake City International Airport', 'Salt Lake City', 'USA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('ANC','Ted Stevens Anchorage International Airport', 'Anchorage', 'PA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('ATL','Hartsfield-Jackson Atlanta International Airport', 'Atlanta', 'TX');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('CLT','Charlotte Douglas International Airport', 'Charlotte', 'NM');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('DEN','Denver International Airport', 'Denver', 'SD');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('DFW','Dallas/Fort Worth International Airport', 'Dallas-Fort Worth', 'GA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('IAH','George Bush Intercontinental Airport', 'Houston', 'MA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('LAS','McCarran International Airport', 'Las Vegas', 'TX');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('LAX','Los Angeles International Airport', 'Los Angeles', 'CA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('MIA','Miami International Airport', 'Miami', 'NJ');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('MSP','Minneapolis-Saint Paul International Airport', 'Minneapolis', 'AK');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('PBI','Palm Beach International Airport', 'West Palm Beach', 'AK');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('PDX','Portland International Airport', 'Portlands', 'LA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('SEA','Seattle-Tacoma International Airport', 'Seattle', 'GA');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('SFO','San Francisco International Airport', 'San Francisco', 'AK');
+INSERT INTO Origin_Airport(IATA_code, Name, City, State)
+VALUES('SLC','Salt Lake City International Airport', 'Salt Lake City', 'NY');
 
 
 INSERT INTO Airline(IATA_code, Name)
@@ -43,36 +43,36 @@ VALUES('UA','United Air Lines Inc.');
 INSERT INTO Airline(IATA_code, Name)
 VALUES('US','US Airways Inc.');
 
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('ANC','Ted Stevens Anchorage International Airport', 'Anchorage', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('ATL','Hartsfield-Jackson Atlanta International Airport', 'Atlanta', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('CLT','Charlotte Douglas International Airport', 'Charlotte', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('DEN','Denver International Airport', 'Denver', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('DFW','Dallas/Fort Worth International Airport', 'Dallas-Fort Worth', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('IAH','George Bush Intercontinental Airport', 'Houston', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('LAS','McCarran International Airport', 'Las Vegas', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('LAX','Los Angeles International Airport', 'Los Angeles', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('MIA','Miami International Airport', 'Miami', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('MSP','Minneapolis-Saint Paul International Airport', 'Minneapolis', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('PBI','Palm Beach International Airport', 'West Palm Beach', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('PDX','Portland International Airport', 'Portlands', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('SEA','Seattle-Tacoma International Airport', 'Seattle', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('SFO','San Francisco International Airport', 'San Francisco', 'USA');
-INSERT INTO Destination_Airport(IATA_code, Name, City, Country)
-VALUES('SLC','Salt Lake City International Airport', 'Salt Lake City', 'USA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('ANC','Ted Stevens Anchorage International Airport', 'Anchorage', 'PA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('ATL','Hartsfield-Jackson Atlanta International Airport', 'Atlanta', 'TX');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('CLT','Charlotte Douglas International Airport', 'Charlotte', 'NM');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('DEN','Denver International Airport', 'Denver', 'SD');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('DFW','Dallas/Fort Worth International Airport', 'Dallas-Fort Worth', 'GA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('IAH','George Bush Intercontinental Airport', 'Houston', 'MA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('LAS','McCarran International Airport', 'Las Vegas', 'TX');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('LAX','Los Angeles International Airport', 'Los Angeles', 'CA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('MIA','Miami International Airport', 'Miami', 'NJ');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('MSP','Minneapolis-Saint Paul International Airport', 'Minneapolis', 'AK');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('PBI','Palm Beach International Airport', 'West Palm Beach', 'AK');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('PDX','Portland International Airport', 'Portlands', 'LA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('SEA','Seattle-Tacoma International Airport', 'Seattle', 'GA');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('SFO','San Francisco International Airport', 'San Francisco', 'AK');
+INSERT INTO Destination_Airport(IATA_code, Name, City, State)
+VALUES('SLC','Salt Lake City International Airport', 'Salt Lake City', 'NY');
 
 
 INSERT INTO Flight_Route(Flight_Number, Origin_Airport, Destination_Airport, Airline)
