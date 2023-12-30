ee9de33baecfc472921e08fac512eca077228f61
Vavrovsky2003 <113840563+Vavrovsky2003@users.noreply.github.com>
2023-12-31 00:07:58 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,35 +1,29 @@
-CREATE TABLE Origin_Airport
+CREATE TABLE Airport
 (
-  IATA_code VARCHAR(3) NOT NULL,
-  Name VARCHAR(100) NOT NULL,
+  Airport_id VARCHAR(3) NOT NULL,
+  Airport_name VARCHAR(100) NOT NULL,
   City VARCHAR(100) NOT NULL,
   State VARCHAR(20) NOT NULL,
-  PRIMARY KEY (IATA_code)
+  PRIMARY KEY (Airport_id)
 );
 
 CREATE TABLE Airline
 (
-  IATA_code VARCHAR(3) NOT NULL,
-  Name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (IATA_code)
+  Airline_id VARCHAR(3) NOT NULL,
+  Airline_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (Airline_id)
 );
 
-CREATE TABLE Destination_Airport
-(
-  IATA_code VARCHAR(3) NOT NULL,
-  Name VARCHAR(100) NOT NULL,
-  City VARCHAR(100) NOT NULL,
-  State VARCHAR(20) NOT NULL,
-  PRIMARY KEY (IATA_code)
-);
 
 CREATE TABLE Flight_Route
 (
+  Flight_id INT NOT NULL,
   Flight_Number INT NOT NULL,
   Origin_Airport VARCHAR(3) NOT NULL,
   Destination_Airport VARCHAR(3) NOT NULL,
   Airline VARCHAR(3) NOT NULL,
-  FOREIGN KEY (Origin_Airport) REFERENCES Origin_Airport(IATA_code),
-  FOREIGN KEY (Destination_Airport) REFERENCES Destination_Airport(IATA_code),
-  FOREIGN KEY (Airline) REFERENCES Airline(IATA_code)
+  PRIMARY KEY (Flight_id),  
+  FOREIGN KEY (Origin_Airport) REFERENCES Airport(Airport_id),
+  FOREIGN KEY (Destination_Airport) REFERENCES Airport(Airport_id),
+  FOREIGN KEY (Airline) REFERENCES Airline(Airline_id)
 )
